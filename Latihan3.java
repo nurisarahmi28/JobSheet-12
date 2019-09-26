bfe220caf7ac662f3e5dfba16fff6ceb8ead307d
Risa <54298936+nurisarahmi28@users.noreply.github.com>
2019-09-26 17:10:57 +0700

    Add files via upload

---
+++ Latihan3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet12;
+
+/**
+ *
+ * @author nuris
+ */
+public class Latihan3 {
+    public static int a(int w, int r, int z) 
+    {
+        return w+r+z;
+    }
+    public static void main(String[] args) {
+        a((int) 1.5,2,3);
+    } 
+}
+    
+
